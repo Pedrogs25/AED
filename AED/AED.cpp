adc25d3d05d7d8a51c5b31c75729a614cf39be8e
Pedro <pedrogs2500@gmail.com>
2025-06-13 18:16:14 -0300

    5 - SaveTarefas implementado

--- AED/AED.cpp
+++ AED/AED.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void AddTarefa(char tarefa[30][2][50], char titulo[20], char descricao[50], int cadastrados, int escolha) {
 	for (int i = cadastrados; i < cadastrados + escolha; i++) {
@@ -25,12 +26,6 @@ void EditTarefa(char tarefa[30][2][50], int indice) {
 }
 
 void DeleteTarefa(char tarefa[30][2][50], int indice, int cadastrados) {
-	int confirma;
-	printf("Tem certeza? (0/1) ");
-	scanf("%i", &confirma);
-
-	if (confirma == 0) {
-
 		for (int i = indice+1; i < cadastrados+1; i++) {
 
 			strcpy(tarefa[i-1][1], tarefa[i][1]);
@@ -38,11 +33,7 @@ void DeleteTarefa(char tarefa[30][2][50], int indice, int cadastrados) {
 
 		}
 
-		cadastrados = cadastrados - 1;
 		printf("Tarefa deletada.\n");
-
-	}
-
 }
 
 void VerTarefas(char tarefa[30][2][50], int cadastrados) {
@@ -52,11 +43,23 @@ void VerTarefas(char tarefa[30][2][50], int cadastrados) {
 	}
 }
 
+void SaveTarefas(char tarefa[30][2][50], int cadastrados) {
+
+	FILE* arquivo;
+
+	arquivo = fopen("tarefas.txt", "w");
+
+	for (int i = 0; i < cadastrados; i++) {
+		fprintf(arquivo, "[%i] %s", i + 1, tarefa[i][0]);
+		fprintf(arquivo, "\n - Descricao -\n%s\n", tarefa[i][1]);
+	}
+}
+
 int main() {
 
 	char tarefa[30][2][50];
 	char titulo[20], descricao[50];
-	int escolha = 0, cadastrados = 0, indice = 0;
+	int escolha = 0, cadastrados = 0, indice = 0, qtd = 0;
 
 	// --------------------------------- //
 
@@ -71,15 +74,15 @@ int main() {
 
 		case 1:
 			printf("Quantidade: ");
-			scanf("%i", &escolha);
+			scanf("%i", &qtd);
 
-			if ((30 - cadastrados) < escolha) {
+			if ((30 - cadastrados) < qtd) {
 				printf("Limite de apenas 30 tarefas!\n");
 			}
 			else {
 
-				AddTarefa(tarefa, titulo, descricao, cadastrados, escolha);
-				cadastrados += escolha;
+				AddTarefa(tarefa, titulo, descricao, cadastrados, qtd);
+				cadastrados += qtd;
 
 			}
 			break;
@@ -96,10 +99,15 @@ int main() {
 			scanf("%i", &indice);
 
 			DeleteTarefa(tarefa, indice-1, cadastrados-1);
+			cadastrados = cadastrados - 1;
 			break;
 
 		case 4:
-			VerTarefas(tarefa, cadastrados-1);
+			VerTarefas(tarefa, cadastrados);
+			break;
+
+		case 5:
+			SaveTarefas(tarefa, cadastrados);
 			break;
 		}
 
